a7451b7ba28123901806a8b4c53ddb97df80a0a3
CBWhiz <CBWhiz@gmail.com>
2012-07-12 23:59:50 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name = "django_qspickle",
+    version = "0.1",
+    packages = ['django_qspickle', ],
+    author='CBWhiz',
+    author_email='CBWhiz@gmail.com',
+    description='Django queryset query pickling',
+    long_description='Allows you to picke a QuerySet (the query), not the results of executing the query.',
+    keywords = "django queryset",
+    url = "https://github.com/CBWhiz/django_qspickle",
+    license = "GNU General Public License (GPL)",
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Framework :: Django',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
